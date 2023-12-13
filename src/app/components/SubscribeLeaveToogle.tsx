807e0f0acf6231c1f8d22d9011e880bf201d4200
marcinWojtczak <marcinwojtczak.pure@gmail.com>
2023-12-13 20:47:41 +0100

    feat: created subscribe buttton component

--- src/app/components/SubscribeLeaveToogle.tsx
+++ src/app/components/SubscribeLeaveToogle.tsx
@@ -1,27 +1,105 @@
-import { Button } from "./ui/Button"
+"use client"
+import { Button } from "@/ui/Button"
 import { useMutation } from "@tanstack/react-query"
-import { TCommunitySubscriptionSchema } from "../lib/validators/community"
+import { TCommunitySubscriptionSchema } from "@/lib/validators/community"
+import axios, { AxiosError } from "axios"
+import { useCustomToast } from "@/hooks/use-custom-toast"
+import { useToast } from "@/hooks/use-toast"
+import { startTransition } from "react"
+import { useRouter } from "next/navigation"
+
 
 interface SubscribeLeaveToggleProps {
-    subredditId: string
+    communityId: string
+    communityName: string
+    isSubscribed: boolean
 } 
 
-const SubscribeLeaveToogle = ( {subredditId}: SubscribeLeaveToggleProps ) => {
+const SubscribeLeaveToogle = ( {communityId, communityName, isSubscribed }: SubscribeLeaveToggleProps ) => {
+ 
+    const { toast } = useToast()
+    const { loginToast } = useCustomToast()
+    const router = useRouter()
 
-    const isSubscribe = false 
-
-    const {} = useMutation({
+    const { mutate: subscribe, isPending: isSubPending } = useMutation({
         mutationFn: async () => {
             const payload: TCommunitySubscriptionSchema = {
-
+              communityId
             }
+
+            const { data } = await axios.post('/api/community/subscribe', payload)
+            return data as string
+        },
+        onError: (error) => {
+          if(error instanceof AxiosError) {
+            if(error.request.status === 401) {
+              return loginToast()
+            }
+          }
+
+          return toast({
+            title: "There was a problem",
+            description: "Something went wrong, pleas try again.",
+            variant: "destructive"
+          })
+        },
+        onSuccess: () => {
+          startTransition(() => {
+            router.refresh()
+          })
+
+          return toast({
+            title: 'Subscribed',
+            description: `You are now subscribed to r/${communityName}`
+          })
         }
     })
 
-  return isSubscribe ? (
-    <Button className='w-full mt-1 mb-4'>Leave Community</Button>
+    const { mutate: unsubscribe, isPending: isUnsubPending } = useMutation({
+      mutationFn: async () => {
+          const payload: TCommunitySubscriptionSchema = {
+            communityId
+          }
+
+          const { data } = await axios.post('/api/community/unsubscribe', payload)
+          return data as string
+      },
+      onError: (error) => {
+        if(error instanceof AxiosError) {
+          if(error.request.status === 401) {
+            return loginToast()
+          }
+        }
+
+        return toast({
+          title: "There was a problem",
+          description: "Something went wrong, pleas try again.",
+          variant: "destructive"
+        })
+      },
+      onSuccess: () => {
+        startTransition(() => {
+          router.refresh()
+        })
+
+        return toast({
+          title: 'Unubscribed',
+          description: `You are now unsubscribed from r/${communityName}`
+        })
+      }
+  })
+
+  return isSubscribed ? (
+    <Button className='w-full mt-1 mb-4'
+      onClick={() => unsubscribe()}
+      isLoading={isUnsubPending}
+    >Leave Community</Button>
   ) : (
-    <Button className='w-full mt-1 mb-4'>Join to post</Button>
+    <Button 
+      onClick={() => subscribe()} 
+      isLoading={isSubPending}
+      className='w-full mt-1 mb-4'
+    >Join to Comunity</Button>
   )
 }
 
