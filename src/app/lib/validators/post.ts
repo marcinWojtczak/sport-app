2c62125fc4b26fe1841f6f46d14ce14a44b62b45
marcinWojtczak <marcinwojtczak.pure@gmail.com>
2024-01-02 13:31:05 +0100

    feat: addevalidation to post fields

--- src/app/lib/validators/post.ts
+++ src/app/lib/validators/post.ts
@@ -1,9 +1,14 @@
 import { z } from "zod"
 
 export const postSchema = z.object({
-    subject: z
+    title: z
         .string()
-        .min(3, { message: "Title must be 3 or more characters long"})
+        .min(3, { message: "Title must be 3 or more characters long."})
+        .max(150, { message: "Ttile can't be longer than 130 characters."}),
+    communityId: z 
+        .string(),
+    content: z
+        .any()
 })
 
 export type TPostSchema = z.infer<typeof postSchema>
