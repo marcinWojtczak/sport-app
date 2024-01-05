5d2599b6ceada22e50d6be2fd4565e7b8c584722
marcinWojtczak <marcinwojtczak.pure@gmail.com>
2024-01-05 13:13:28 +0100

    feat: add functionality to create community
    -Implemented a new route for creating posts in
    'src/app/api/communit/route.ts'
    -Created TCommunitySchema and communitySchema in 'src/app/api/lib/validators/community.ts'

---
+++ src/app/s/[slug]/page.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { getServerSession } from 'next-auth'
+import { authOptions } from "@/lib/auth"
+import { db } from '@/app/lib/db'
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/app/config'
+import { notFound } from 'next/navigation'
+import CreatePost from '@/app/components/CreatePost'
+
+interface PageProps {
+    params: {
+        slug: string
+    }
+}
+
+const Page = async ({ params }: PageProps) => {
+    const { slug } = params
+   
+  console.log(slug)
+    const session = await getServerSession(authOptions)
+
+    const community = await db.community.findFirst({
+      where: { title: slug },
+      include: {
+        posts: {
+          include: {
+            author: true,
+            comments: true,
+            community: true
+          }
+        }
+      },
+      
+      take: INFINITE_SCROLLING_PAGINATION_RESULTS
+    })
+
+    if(!community) return notFound()
+
+    return (
+    <div>
+      <h1 className='font-bold mb-4'>r/{community.title}</h1>
+      <CreatePost session={session}/>
+    </div>
+  )
+}
+
+export default Page
