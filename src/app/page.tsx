3f8f9f7cbca4610b1be7bf390de878c119ea5995
marcinWojtczak <marcinwojtczak.pure@gmail.com>
2024-03-06 21:17:55 +0100

    Feat: Fetch posts form db
    Fetch post from db and added PostFeed component with initialPost and
    user props
    Please enter the commit message for your changes. Lines starting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,34 @@
-import { db } from "./lib/db"
-
+import PostFeed from "@/components/PostFeed"
+import { getServerSession } from 'next-auth'
+import { authOptions } from "@/lib/auth"
+import { db } from '@/app/lib/db'
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/app/config'
 
 
 
 export default async function Home() {
   
+  const session = await getServerSession(authOptions)
+  
+  const posts = await db.post.findMany({
+    include: {
+      comments: true
+    },
+    orderBy: {
+      createdAt: 'desc'
+    },
+    take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+  })
+  
   
   
 
   return (
-      <div className='flex flex-col gap-4 sm:container max-w-7xl mx-auto h-full pt-12'>
-        
+      <div className='flex flex-col items-center sm:container max-w-7xl mx-auto h-full pt-12'>
+        <PostFeed 
+          initialPosts={posts}
+          user={session?.user}
+          />
       </div>
   )
 }
