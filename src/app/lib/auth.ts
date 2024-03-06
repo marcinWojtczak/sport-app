98880a9aa9af425518d50c6678785415a203a44e
marcinWojtczak <marcinwojtczak.pure@gmail.com>
2024-03-06 14:16:30 +0100

    Feat: Installed @mantine/hooks

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -5,7 +5,7 @@ import GoogleProvider from "next-auth/providers/google"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { db } from "@/lib/db"
-import { compare } from "bcrypt"
+import { compare } from "bcrypt";
 import { nanoid } from 'nanoid'
 
 
