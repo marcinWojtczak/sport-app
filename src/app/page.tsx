0a912ae9cdfd921843029d22b9ae45f12386c240
marcinWojtczak <marcinwojtczak.pure@gmail.com>
2024-03-19 18:02:52 +0100

    Feat: Modified the db data fetching from post to event'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,30 @@
-import PostFeed from "@/components/PostFeed"
+import EventFeed from "@/app/components/EventFeed"
 import { getServerSession } from 'next-auth'
 import { authOptions } from "@/lib/auth"
 import { db } from '@/app/lib/db'
-import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/app/config'
-
 
 
 export default async function Home() {
   
   const session = await getServerSession(authOptions)
-  
-  const posts = await db.post.findMany({
+
+  const events = await db.event.findMany({
     include: {
-      comments: true
-    },
-    orderBy: {
-      createdAt: 'desc'
-    },
-    take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+      posts: {
+        include: {
+          media: true
+        }
+      }
+    }
   })
   
-  
-  
-
   return (
       <div className='flex flex-col items-center sm:container max-w-7xl mx-auto h-full pt-12'>
-        <PostFeed 
-          initialPosts={posts}
+        <h2 className='font-medium'>Today's events</h2>
+        <EventFeed 
           user={session?.user}
-          />
+          events={events}
+        />
       </div>
   )
 }
