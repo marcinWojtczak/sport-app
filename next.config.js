8880f109472af21c45723f514ac8889ab3aaa09f
marcinWojtczak <marcinwojtczak.pure@gmail.com>
2024-03-19 16:05:58 +0100

    Feat: Added s3.eu-central-1.amazonaws.com domain to the list of allowed domains for images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-      domains: ['uploadthing.com', 'lh3.googleusercontent.com', "avatars.githubusercontent.com"],
+      domains: ['uploadthing.com', 'lh3.googleusercontent.com', "avatars.githubusercontent.com", 's3.eu-central-1.amazonaws.com'],
       },
 }
 
